b3c394c78592214833e8f97ecb4af684d95b58bb
AshokAshok24 <ashok246445@gmail.com>
2024-01-26 16:33:56 +0530

    modified pool.j and index.js

---
+++ helpers/config/pool.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+
+// Connection String For Mysql
+
+const dbconnection = mysql.createConnection({
+    user: process.env.MYSQL_USER,
+    host: process.env.MYSQL_HOST,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+// Check if Database is Connected or Not
+
+dbconnection.connect((err) => {
+    if (err) {
+        console.log(`Error connecting to MySQL database:`, err);
+        return;
+    } else {
+        console.log(`Connected To Database : ${process.env.MYSQL_DATABASE}`);
+    }
+})
+
+dbconnection.on('error', (err) => {
+    console.error('MySQL database error:', err);
+})
+
+
+// Export Database Connection
+
+module.exports = dbconnection
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express=require('express');
-const app=express();
-const dbconnection = require('./helpers/config');
+const express = require('express');
+const app = express();
+const dbconnection = require('./helpers/config/pool');
 require('dotenv').config()
 
 app.use(express.json());
 
 
 
-app.get('/',(req,res)=>{
-    
-    dbconnection.query(`select * from users`,(err,result)=>{
+app.get('/', (req, res) => {
+
+    dbconnection.query(`select * from users`, (err, result) => {
         res.send(result)
     })
 })
 
 // For User Controller
 
-const users=require('./controller/usercontroller/users');
-app.use('/users',users)
+const users = require('./controller/usercontroller/users');
+app.use('/users', users)
 
 
 
-const port=process.env.PORT
+const port = process.env.PORT
 // For Listining the server
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Running in the port http://localhost:${port}`);
 })
